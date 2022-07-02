80ea861cb153c307f3f310b6f830f28665df8589
Stephane Barakat <sb@Office-Mac.local>
2022-07-02 10:17:51 -0400

    update

---
+++ src/songs/blackHoleSun.js
@@ -0,0 +1,69 @@
+export const blackHoleSun = {
+  name: "Black Hole Sun",
+  slug: "black-hole-sun",
+  artist: "Soundgarden",
+  year: "1994",
+  studio: "Bad Animals",
+  location: "Seattle, WA",
+  bpm: 105,
+  start: 0,
+  end: 334,
+  tracks: [
+    {
+      name: "Kick",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/01-kick.mp3",
+    },
+    {
+      name: "Snare",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/02-snare-1.mp3",
+    },
+    {
+      name: "Snare 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/03-snare-2.mp3",
+    },
+    {
+      name: "OverHeads",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/04-overheads.mp3",
+    },
+    {
+      name: "Room",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/05-room.mp3",
+    },
+    {
+      name: "Bass",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/06-bass.mp3",
+    },
+    {
+      name: "Gtr 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/07-guitar.mp3",
+    },
+    {
+      name: "Gtr 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/08-guitar-2.mp3",
+    },
+    {
+      name: "Vox 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/09-vox.mp3",
+    },
+    {
+      name: "Vox 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/10-vox-2.mp3",
+    },
+    {
+      name: "Back Vox 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/11-back.mp3",
+    },
+    {
+      name: "Back Vox 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/12-back-vox-2.mp3",
+    },
+    {
+      name: "Crowd 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/13-crowd.mp3",
+    },
+    {
+      name: "Crowd 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/blackHoleSun/14-crowd-2.mp3",
+    },
+  ],
+};
---
+++ src/songs/creep.js
@@ -0,0 +1,61 @@
+export const creep = {
+  name: "Creep",
+  artist: "Radiohead",
+  year: "1992",
+  studio: "Chipping Norton Recording Studios",
+  location: "Oxfordshire, England",
+  slug: "creep",
+  bpm: 92,
+  start: 2,
+  end: 251,
+  tracks: [
+    {
+      name: "Kick",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/kick.mp3",
+    },
+    {
+      name: "Snare 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/snare.mp3",
+    },
+    {
+      name: "Snare 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/snare2.mp3",
+    },
+    {
+      name: "Overheads L",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/overheads.mp3",
+    },
+    {
+      name: "Overheads R",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/overheads2.mp3",
+    },
+    {
+      name: "Bass",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/bass.mp3",
+    },
+    {
+      name: "Gtr 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/guitar.mp3",
+    },
+    {
+      name: "Gtr 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/guitar2.mp3",
+    },
+    {
+      name: "Gtr + Piano 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/guitar+piano.mp3",
+    },
+    {
+      name: "Gtr + Piano 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/guitar+piano2.mp3",
+    },
+    {
+      name: "Vox 1",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/vocal.mp3",
+    },
+    {
+      name: "Vox 2",
+      path: "https://ioxpcmpvgermtfqxwykx.supabase.co/storage/v1/object/public/songs/creep/vocal2.mp3",
+    },
+  ],
+};
