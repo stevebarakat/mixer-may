a55ba01fcc1444ef7193b8a8ea6ae8391986c067
Stephane Barakat <sb@Office-Mac.local>
2022-07-03 16:15:57 -0400

    bus one

---
+++ src/components/Channels/Bus.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+import VuMeter from "./VuMeter";
+import { dBToPercent } from "../../utils/scale";
+import useMeter from "../../hooks/useMeter";
+
+function Bus({
+  index,
+  busChannels,
+  busChoices,
+  handleSetBusChoices,
+  bussesActive,
+}) {
+  const [masterVol, setMasterVol] = useState(0);
+  const busOneActiveBool = bussesActive.some((bus) => bus === true);
+
+  function changeMasterVolume(e) {
+    if (!busOneActiveBool) return;
+    const id = e.target.id;
+    const value = parseInt(e.target.value, 10);
+    const v = Math.log(value + 101) / Math.log(113);
+    const sv = dBToPercent(v);
+    setMasterVol(Math.round(sv));
+    busChannels[id].set({ volume: sv });
+  }
+
+  const masterMeterVal = useMeter([busChannels]);
+
+  const arr = new Array(2).fill(null);
+  const options = arr.map((_, i) => {
+    return (
+      <div key={i} style={{ display: "flex", flexDirection: "column" }}>
+        <select
+          id={i}
+          onChange={(e) => {
+            busChoices[i] = e.target.value;
+            handleSetBusChoices([...busChoices]);
+          }}
+          className="effect-select"
+          disabled={!busOneActiveBool}
+        >
+          <option value={`bs${i + 1}-fx${i + 1}`}>{`FX${i + 1}`}</option>
+          <option value="reverb">Reverb</option>
+          <option value="delay">Delay</option>
+          <option value="chorus">Chorus</option>
+          <option value="chebyshev">Chebyshev</option>
+          <option value="pitch-shift">PitchShift</option>
+          <option value="compressor">Compressor</option>
+        </select>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {options}
+      <div
+        className="fader-wrap"
+        style={{
+          padding: "12px 0 0 0",
+          margin: "0 0 0 16px",
+        }}
+      >
+        <div className="window">
+          <input
+            disabled
+            type="text"
+            className="level-val"
+            value={masterVol + " db"}
+          />
+        </div>
+        <div className="levels-wrap">
+          <VuMeter meterValue={masterMeterVal} height={450} width={12.5} />
+        </div>
+        <div className="master-vol-wrap">
+          <input
+            className="master-volume"
+            type="range"
+            min={-100}
+            max={12}
+            defaultValue={-32}
+            step="0.1"
+            onChange={changeMasterVolume}
+          />
+        </div>
+        <div className="track-labels">
+          <span className="track-name">Bus {index}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Bus;
--- src/components/Mixer.jsx
+++ src/components/Mixer.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect, useRef } from "react";
-import {
-  loaded,
-  Player,
-  EQ3,
-  Channel,
-  Transport as t,
-  Destination,
-  Volume,
-} from "tone";
+import { useState, useRef } from "react";
+import { Destination, Volume } from "tone";
 import Controls from "./Transport/Controls";
 
 import MasterVol from "./Channels/MasterVol";
-import Bus1 from "./Channels/Bus1";
-import Bus2 from "./Channels/Bus2";
+import Bus from "./Channels/Bus";
 import ChannelStrip from "./Channels/ChannelStrips";
 import Loader from "./Loader";
 import useSetFxType from "../hooks/useSetFxType";
@@ -20,53 +11,30 @@ import useChannelStrip from "../hooks/useChannelStrip";
 
 function Mixer({ song }) {
   const tracks = song.tracks;
-  const busOneChannel = useRef(new Volume().toDestination());
-  const busTwoChannel = useRef(new Volume().toDestination());
+  const busChannels = useRef(new Volume().toDestination());
 
   const [busChoices, setBusChoices] = useState([]);
   const handleSetBusChoices = (value) => setBusChoices(value);
-  const [busOneActive, setBusOneActive] = useState([]);
-  const [busTwoActive, setBusTwoActive] = useState([]);
+  const [bussesActive, setBussesActive] = useState([]);
 
   const [channels, eqs, isLoaded] = useChannelStrip({ tracks });
 
-  const [fxControls] = useSetFxType(busChoices, busOneChannel);
+  const [fxControls] = useSetFxType(busChoices, busChannels);
 
   function toggleBusOne(e) {
     const id = parseInt(e.target.id[0], 10);
     channels.current.forEach((channel, i) => {
       if (id === i) {
         if (e.target.checked) {
-          busOneActive[id] = true;
-          setBusOneActive([...busOneActive]);
+          bussesActive[id] = true;
+          setBussesActive([...bussesActive]);
           channels.current[id].connect(Destination);
           channels.current[id].disconnect(Destination);
-          channels.current[id].connect(busOneChannel.current);
+          channels.current[id].connect(busChannels.current);
         } else {
-          busOneActive[id] = false;
-          setBusOneActive([...busOneActive]);
-          channels.current[id].disconnect(busOneChannel.current);
-          channels.current[id].connect(Destination);
-        }
-      }
-    });
-  }
-
-  function toggleBusTwo(e) {
-    const id = parseInt(e.target.id[0], 10);
-    channels.current.forEach((channel, i) => {
-      if (id === i) {
-        if (e.target.checked) {
-          busTwoActive[id] = true;
-          setBusTwoActive(busTwoActive);
-          channels.current[id].connect(Destination);
-          channels.current[id].disconnect(Destination);
-          channels.current[id].connect(busTwoChannel.current);
-        } else {
-          busTwoActive[id] = false;
-          setBusTwoActive(busTwoActive);
-          channels.current[id].connect(busTwoChannel.current);
-          channels.current[id].disconnect(busTwoChannel.current);
+          bussesActive[id] = false;
+          setBussesActive([...bussesActive]);
+          channels.current[id].disconnect(busChannels.current);
           channels.current[id].connect(Destination);
         }
       }
@@ -97,19 +65,12 @@ function Mixer({ song }) {
               track={track}
               tracks={tracks}
               toggleBusOne={toggleBusOne}
-              toggleBusTwo={toggleBusTwo}
             />
           );
         })}
-        <Bus1
-          busOneActive={busOneActive}
-          busOneChannel={busOneChannel.current}
-          busChoices={busChoices}
-          handleSetBusChoices={handleSetBusChoices}
-        />
-        <Bus2
-          busTwoActive={busTwoActive}
-          busTwoChannel={busTwoChannel.current}
+        <Bus
+          bussesActive={bussesActive}
+          busChannels={busChannels.current}
           busChoices={busChoices}
           handleSetBusChoices={handleSetBusChoices}
         />
